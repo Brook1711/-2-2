1a1947b3d04a6f257a03b6e0bb9ab3cd0c26e97f
brook1711 <670396323@qq.com>
2018-04-02 20:52:30 +0800

    一年当中第几天

---
+++ 第二学期2、2/源.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>  
+int main()
+{
+	int days[] = { 0,31,28,31,30,31,30,31,31,30,31,30 };
+	int y, m, d, i, sum;
+	while (~scanf("%d/%d/%d", &y, &m, &d))
+	{
+		sum = 0;
+		for (i = 0; i<m; i++)
+			sum += days[i];
+		sum += d;
+		if ((y % 4 == 0 && y % 100 != 0 || y % 400 == 0) && m >= 2)
+			sum += 1;
+		printf("%d\n", sum);
+	}
+	return 0;
+}
